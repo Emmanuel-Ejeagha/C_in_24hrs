e1f576b9c0474f6c7a178ae6ab31a2bc37bf67fa
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-03-13 13:06:18 +0000

    while loop

---
+++ hour_7/while_-.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int c;
+
+	c = ' ';
+
+	printf("Enter a character:\n(enter x to exit)\n");
+	while (c != 'x')
+	{
+		c = getc(stdin);
+		putchar(c);
+	}
+	printf("\nOut of the while loop. Bye!\n");
+	return 0;
+ 	       
+}
