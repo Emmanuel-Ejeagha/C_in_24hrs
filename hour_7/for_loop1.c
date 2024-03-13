13a0966a2cc3442347d5daf8cd78b5f784e0e33e
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-03-13 13:11:30 +0000

    for loop 1 file

---
+++ hour_7/for_loop1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Add multiple expressions to the for statement
+
+int main(void)
+{
+	int i, j;
+
+	for (i=0, j=8; i<8; i++, j--)
+		printf("%d  +  %d = %d\n", i, j, i+j);
+	return(0);
+}
