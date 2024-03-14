dd95a5cc302b3303e7a051763793fe21253cbdc0
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-03-14 22:00:34 +0000

    logical OR

---
+++ hour_8/logical_or.c
@@ -0,0 +1,13 @@
+// using logical OR operator
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+
+	printf("Enter a single digit that can be divided\n both 2 and 3:\n");
+	for (num = 1; (num%2 != 0) || (num%3 != 0);)
+		num = getchar() - '0';
+	printf("You got such a number: %d\n", num);
+	return(0);
+}
